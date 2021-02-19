1142a6621244c87fe64096069ad7e550d42d3a6e
unknown <buddhat645@gmail.com>
2021-02-19 16:41:49 +0545

    yetikai ho

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,47 @@
 import React from "react";
 import Home from "./Home";
+import Contact from "./Contact";
+import About from "./About";
+import Blog from "./Blog";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 const Header = () => {
   return (
     <div>
-      <Home></Home>
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/contact">Contact</Link>
+              </li>
+              <li>
+                <Link to="/about">About</Link>
+              </li>
+              <li>
+                <Link to="/blog">Blog</Link>
+              </li>
+            </ul>
+          </nav>
+          <Switch>
+            <Route path="/about">
+              <About></About>
+            </Route>
+            <Route path="/blog">
+              <Blog></Blog>
+            </Route>
+            <Route path="/contact">
+              <Contact></Contact>
+            </Route>
+            <Route path="/">
+              <Home></Home>
+            </Route>
+          </Switch>
+        </div>
+      </Router>
     </div>
   );
 };
